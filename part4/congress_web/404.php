9e17f1a5a056166eb39be786397098e30fc83956
Weston Ortiz <weston@wortiz.com>
2017-05-08 16:50:25 -0600

    Add a individual bill page

---
+++ part4/congress_web/404.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <div class="twelve columns">
+        <h2>Error 404 Not Found</h2>
+    </div>
+</div>
---
+++ part4/congress_web/bill.php
@@ -0,0 +1,53 @@
+<?php
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+} else {
+    http_response_code(404);
+    $Title = "Error 404, Bill not found";
+    include('header.php');
+    include('404.php'); // provide your own HTML for the error page
+    include('footer.php');
+    die();
+}
+
+$Title = "Bill " . $id;
+
+include('header.php');
+$stmt = $db->prepare("SELECT id, type, title, popular_title, short_title, status, introduction_date, summary, congress, number FROM Bill WHERE id=?");
+
+$stmt->bind_param("s", $id);
+
+$stmt->execute();
+
+$stmt->store_result();
+
+$stmt->bind_result($bill_id, $type, $title, $popular_title, $short_title, $status, $introduction_date, $summary, $congress, $number);
+
+$stmt->fetch();
+
+?>
+
+<div class="row">
+    <div class="twelve column">
+        <h4><?= $title ?></h4>
+        <dl>
+            <dt>id</dt>
+            <dd><?= $id ?></dd>
+            <dt>type</dt>
+            <dd><?= $type ?></dd>
+            <dt>status</dt>
+            <dd><?= $status ?></dd>
+            <dt>introduction_date</dt>
+            <dd><?= $introduction_date ?></dd>
+            <dt>congress</dt>
+            <dd><?= $congress ?></dd>
+            <dt>number</dt>
+            <dd><?= $number ?></dd>
+            <dt>summary</dt>
+            <dd><?= $summary ?></dd>
+        </dl>
+    </div>
+</div>
+
+<?php include('footer.php'); ?>
