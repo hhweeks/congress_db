38c4a7a63adb75fd65e6332498331fe5067d81f5
Weston Ortiz <weston@wortiz.com>
2017-05-09 21:42:50 -0600

    Add some general demographics

--- part4/congress_web/demographics.php
+++ part4/congress_web/demographics.php
@@ -8,7 +8,6 @@ include('header.php'); ?>
 
 <div class="row">
     <div class="four columns">
-    <h4>Majority party by state</h4>
 
 <?php
 $sql =<<<EOQ
@@ -43,8 +42,84 @@ $columnname = array('state', 'party', 'count');
             </tbody>
         </table>
     <?php $r->close(); ?>
+
     </div>
+    <div class="four columns">
+        <h4>Counts of genders</h4>
+
+<?php
+$sql =<<<EOQ
+
+select gender, count(gender) from (select distinct bioguide_id, start, end, state, gender from Legislator natural join Term
+where start < (select end from Congress where id = $congress) and NOT (end <= (select begin from Congress where id = $congress))) as curr GROUP BY gender ORDER BY gender;
+EOQ;
+
+$r = $db->query($sql);
+
+$columnname = array('gender', 'count');
+?>
+        <table>
+            <thead>
+                <tr>
+                <?php for ($i=0; $i<2; $i++) { ?>
+                    <th>
+                        <?= $columnname[$i]; ?>
+                    </th>
+                <?php } //end for ?>
+                </tr>
+            </thead>
+            <tbody>
+            <?php while ($row = $r->fetch_array()) { ?>
+                <tr>
+                <?php for ($i=0; $i<2; $i++) { ?>
+                   <td><?= $row[$i]; ?></td>
+                <?php } ?>
+                </tr>
+            <?php } //end while ?>
+            </tbody>
+        </table>
+    <?php $r->close(); ?>
+
+    </div>
+
+        <div class="four columns">
+        <h4>Counts of parties</h4>
+
+<?php
+$sql =<<<EOQ
+
+select party, count(party) from (select distinct bioguide_id, start, end, state, party from Legislator natural join Term
+where start < (select end from Congress where id = $congress) and NOT (end <= (select begin from Congress where id = $congress))) as curr GROUP BY party ORDER BY party;
+EOQ;
+
+$r = $db->query($sql);
+
+$columnname = array('party', 'count');
+?>
+        <table>
+            <thead>
+                <tr>
+                <?php for ($i=0; $i<2; $i++) { ?>
+                    <th>
+                        <?= $columnname[$i]; ?>
+                    </th>
+                <?php } //end for ?>
+                </tr>
+            </thead>
+            <tbody>
+            <?php while ($row = $r->fetch_array()) { ?>
+                <tr>
+                <?php for ($i=0; $i<2; $i++) { ?>
+                   <td><?= $row[$i]; ?></td>
+                <?php } ?>
+                </tr>
+            <?php } //end while ?>
+            </tbody>
+        </table>
+    <?php $r->close(); ?>
 
     </div>
 </div>
+
+
 <?php include('footer.php'); ?>
